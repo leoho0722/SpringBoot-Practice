09dc16805aa1e75cd41b817a501780904981671a
leoho0722 <leo160918@gmail.com>
2022-05-02 20:21:31 +0800

    Spring Boot @RestController 葉子長出來了

--- Demo0430/src/main/java/com/leoho/demo0430/model/Users.java
+++ Demo0430/src/main/java/com/leoho/demo0430/model/Users.java
@@ -1,7 +1,24 @@
 package com.leoho.demo0430.model;
 
 public class Users {
+
     private String email;
 
     private String password;
+
+    public void setEmail(String emailStr) {
+        this.email = emailStr;
+    }
+
+    public void setPassword(String passwordStr) {
+        this.password = passwordStr;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
